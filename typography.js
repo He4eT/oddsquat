a5680ec927b2e7ad5eb667ad81991274eb39bfc4
He4eT <He4eTHb1u@gmail.com>
2020-10-30 15:32:12 +0500

    typography: params

--- typography.js
+++ typography.js
@@ -5,6 +5,20 @@ const TYPOGRAPHY_CSS_FILEPATH =
   './src/assets/css/typography.css'
 
 const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.5,
+  scaleRatio: 3,
+  googleFonts: [{
+    name: 'Open Sans Condensed',
+    styles: ['700']
+  }, {
+    name: 'Open Sans',
+    styles: ['400', '400i', '700', '700i'],
+  }],
+  headerFontFamily: ['Open Sans Condensed', 'sans-serif'],
+  bodyFontFamily: ['Open Sans', 'sans-serif'],
+  headerColor: 'hsl(0, 0%, 0%, 0.7)',
+  bodyColor: 'hsl(0, 0%, 0%, 0.8)'
 })
 
 void fs.writeFile(

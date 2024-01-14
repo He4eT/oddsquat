231925c47c98ae3999cf3dc6b33e2bf2ba0fd66c
He4eT <He4eT@oddsquat.org>
2024-01-14 22:18:51 +0100

    tools: release script for github pages

---
+++ tools/gh.release.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+GIT_ROOT=`git rev-parse --show-toplevel`
+
+CURRENT_TIMESTAMP=`date +"%Y-%m-%d-%H%M%S"`
+
+RELEASE_BRANCH='release'
+BUILD_DIR='docs'
+
+git checkout master
+git branch -D $RELEASE_BRANCH
+git checkout -b $RELEASE_BRANCH
+
+rm -rf "$GIT_ROOT/$BUILD_DIR"
+npm run build
+
+git add "$GIT_ROOT/$BUILD_DIR"
+git commit -m "release: $CURRENT_TIMESTAMP"
+
+git push -f origin $RELEASE_BRANCH
+
+rm -rf "$GIT_ROOT/$BUILD_DIR"
+git checkout master

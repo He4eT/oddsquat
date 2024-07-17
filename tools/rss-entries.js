bac6b437340bcde922db028c82f9f895d9a1eb88
He4eT <He4eT@oddsquat.org>
2024-07-17 07:32:16 +0200

    tools: rss: update entries

--- tools/rss-entries.js
+++ tools/rss-entries.js
@@ -1,13 +1,24 @@
+/*
+ * date
+ * url
+ * title
+ * description
+ */
 const feed = [[
-  '2021-10-02',
-  'https://oddsquat.org/1',
-  'Second Item Title',
-  'Use this for the content. It can include html.'
+  '2024-01-15',
+  'https://oddsquat.org/posts/2024/selfhosted_llm/',
+  '[RU] selfhosted LLM',
+  'Персональные LLM в docker-контейнере на твоём компьютере.'
 ], [
-  '2021-10-01',
-  'https://oddsquat.org/2',
-  'First Item Title',
-  'Use this for the content. It can include html.'
+  '2021-11-18',
+  'https://oddsquat.org/posts/2020/typographic_linter/',
+  '[RU] typographic linter',
+  'Prettier для текста. Автоматизация рутинной типографики.'
+], [
+  '2020-11-08',
+  'https://oddsquat.org/posts/2020/initial_post/',
+  '[RU] initial post',
+  'Первый пост в этом фэнзине, рассказывающий о его внутреннем устойстве.'
 ]]
 
 module.exports = { feed }
